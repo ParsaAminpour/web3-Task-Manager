665cdfc9508d7db7801da7ad05a1c1eae444d365
Parsa <parsa.aminpour@gmail.com>
2023-08-28 17:03:24 +0330

    commit for branch

--- TaskManagerContract/contracts/TaskManager.sol
+++ TaskManagerContract/contracts/TaskManager.sol
@@ -14,12 +14,11 @@ import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
-import { MessageHashUtils } from "./Signature.sol";
+import {MessageHashUtils} from "./Signature.sol";
 // import { Signature } from "./Signature.sol";
 import "./TaskToken.sol";
 
-    
-contract TaskManager is Ownable,ReentrancyGuard, TaskToken {
+contract TaskManager is Ownable, ReentrancyGuard, TaskToken {
     using SignatureChecker for bytes32;
     using MessageHashUtils for bytes32;
     using SafeMath for uint256;
@@ -27,13 +26,11 @@ contract TaskManager is Ownable,ReentrancyGuard, TaskToken {
     using Math for uint256;
     using Address for address;
     using Strings for string;
-        
 
     string public TaskTitle;
     TaskToken public token;
     address public immutable OWNER;
-    uint public constant CONST_REWARD_AMOUNT = 10 * (10**18);
-
+    uint256 public constant CONST_REWARD_AMOUNT = 10 * (10 ** 18);
 
     enum TASK_STATUS {
         COMPLETED,
@@ -52,11 +49,11 @@ contract TaskManager is Ownable,ReentrancyGuard, TaskToken {
     }
 
     mapping(address => mapping(address => TaskDetails)) public TaskGrant;
-    mapping(uint => TaskDetails) public IdOfTasks;
+    mapping(uint256 => TaskDetails) public IdOfTasks;
     mapping(bytes32 => address) private SignedMessageToOwner;
     mapping(address => TaskDetails[]) public TasksOwnershipList;
     mapping(address => uint256) public TaskOwnerBudget;
-    mapping(uint => bool) public TaskIdActivate;
+    mapping(uint256 => bool) public TaskIdActivate;
     mapping(string => bool) public TaskMessageUsed;
 
     address[] public blackList;
@@ -75,12 +72,11 @@ contract TaskManager is Ownable,ReentrancyGuard, TaskToken {
         OWNER = msg.sender;
     }
 
-    function _getHashValue(address _owner, string memory _msg) internal view returns(bytes32 hashed) {
+    function _getHashValue(address _owner, string memory _msg) internal view returns (bytes32 hashed) {
         return keccak256(abi.encodePacked(_msg, msg.sender));
     }
 
-
-    modifier onlyTaskOwner(uint _task_id, uint8 _v, bytes32 _r, bytes32 _s) {
+    modifier onlyTaskOwner(uint256 _task_id, uint8 _v, bytes32 _r, bytes32 _s) {
         require(IdOfTasks[_task_id].task_owner == msg.sender, "You are NOT the task owner");
 
         // Check signer verification
@@ -92,24 +88,24 @@ contract TaskManager is Ownable,ReentrancyGuard, TaskToken {
         bytes32 verified_signer_message = hashed_message_for_verify.toEthSignedMessageHash();
 
         address signer = ecrecover(verified_signer_message, _v, _r, _s);
-        
+
         require(SignedMessageToOwner[verified_signer_message] == signer, "We have not your task sign");
 
-        if(signer == msg.sender && verified_signer_message == signed_message_for_verify) {
+        if (signer == msg.sender && verified_signer_message == signed_message_for_verify) {
             blackList.push(msg.sender);
             revert("You are Not owner");
-        } else emit SignedMessageVerified(msg.sender, verified_signer_message);
+        } else {
+            emit SignedMessageVerified(msg.sender, verified_signer_message);
+        }
 
         _;
     }
-
-
 
     function CreateTask(string memory _task_msg, uint256 _task_completed_date) external returns (bool created) {
         require(!(_task_msg.equal("") && _task_completed_date == block.timestamp), "invalid values inserted");
         require(!(TaskMessageUsed[_task_msg]), "This message of task has already used");
 
-        uint task_id_gen = uint256(keccak256(abi.encodePacked(msg.sender)));
+        uint256 task_id_gen = uint256(keccak256(abi.encodePacked(msg.sender)));
 
         bytes32 hashed = _getHashValue(msg.sender, _task_msg);
         bytes32 SignedMessageGen = hashed.toEthSignedMessageHash();
@@ -133,74 +129,90 @@ contract TaskManager is Ownable,ReentrancyGuard, TaskToken {
         created = true;
     }
 
-
-    function _FindTaskFromTaskOwnershipList(uint _task_id_for_looking) internal view returns(uint) {
+    function _FindTaskFromTaskOwnershipList(uint256 _task_id_for_looking) internal view returns (uint256) {
         require(_task_id_for_looking < TasksOwnershipList[msg.sender].length, "invalid task id");
 
-        for(uint j=0; j < TasksOwnershipList[msg.sender].length; j++) {
-            if(TasksOwnershipList[msg.sender][j].task_id == _task_id_for_looking) return j;
+        for (uint256 j = 0; j < TasksOwnershipList[msg.sender].length; j++) {
+            if (TasksOwnershipList[msg.sender][j].task_id == _task_id_for_looking) return j;
         }
         revert("Not found");
     }
 
-
-    /** NOTE: v, r, s will fetch via ethers js */
-    function removeTask(uint _task_id_for_remove, uint8 _v, bytes32 _r, bytes32 _s) external onlyTaskOwner(_task_id_for_remove, _v, _r, _s) returns(bool removed) {
+    /**
+     * NOTE: v, r, s will fetch via ethers js
+     */
+    function removeTask(uint256 _task_id_for_remove, uint8 _v, bytes32 _r, bytes32 _s)
+        external
+        onlyTaskOwner(_task_id_for_remove, _v, _r, _s)
+        returns (bool removed)
+    {
         require(TaskIdActivate[_task_id_for_remove], "Task has already canceled");
         TaskDetails memory removing_task = IdOfTasks[_task_id_for_remove];
         require(removing_task.task_status != TASK_STATUS.CANCELED, "task has already canceled");
 
         removing_task.task_status = TASK_STATUS.CANCELED;
 
-        uint task_index = _FindTaskFromTaskOwnershipList(_task_id_for_remove);
-        TasksOwnershipList[msg.sender][task_index] = TasksOwnershipList[msg.sender][TasksOwnershipList[msg.sender].length-1];
+        uint256 task_index = _FindTaskFromTaskOwnershipList(_task_id_for_remove);
+        TasksOwnershipList[msg.sender][task_index] =
+            TasksOwnershipList[msg.sender][TasksOwnershipList[msg.sender].length - 1];
         TasksOwnershipList[msg.sender].pop();
 
         emit TaskRemoved(removing_task.task_owner, removing_task.task_id);
         removed = true;
     }
 
-
-    /** NOTE: v, r, s will fetch via ethers js */
-    /**NOTE: for passing value input if you don't want to update, pass zero value if that type */
-    function updateTask(string memory _new_msg, uint _new_time, uint _task_id_for_update,uint8 _v, bytes32 _r, bytes32 _s) 
-        external onlyTaskOwner(_task_id_for_update,  _v, _r, _s) returns(bool updated) {
-
+    /**
+     * NOTE: v, r, s will fetch via ethers js
+     */
+    /**
+     * NOTE: for passing value input if you don't want to update, pass zero value if that type
+     */
+    function updateTask(
+        string memory _new_msg,
+        uint256 _new_time,
+        uint256 _task_id_for_update,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external onlyTaskOwner(_task_id_for_update, _v, _r, _s) returns (bool updated) {
         TaskDetails storage updating_task = IdOfTasks[_task_id_for_update];
-        uint task_idx = _FindTaskFromTaskOwnershipList(_task_id_for_update);
+        uint256 task_idx = _FindTaskFromTaskOwnershipList(_task_id_for_update);
 
         require(!(updating_task.task_message.equal(_new_msg)), "message not new");
         require(_new_time == updating_task.task_complete_period, "complete timestamp is not new");
         require(_new_time != block.timestamp || _new_time > block.timestamp, "new time is not for future");
 
-        if(!(_new_msg.equal(""))) {
+        if (!(_new_msg.equal(""))) {
             updating_task.task_message = _new_msg;
             TasksOwnershipList[msg.sender][task_idx].task_message = _new_msg;
-            updated=true;
-
-        } else if(!(_new_time != 0 && _new_time == block.timestamp)) {
+            updated = true;
+        } else if (!(_new_time != 0 && _new_time == block.timestamp)) {
             updating_task.task_complete_period = _new_time;
             TasksOwnershipList[msg.sender][task_idx].task_complete_period = _new_time;
-            updated=true;
-
-        } else if(!(_new_msg.equal("")) && !(_new_time != 0 && _new_time == block.timestamp)) {
+            updated = true;
+        } else if (!(_new_msg.equal("")) && !(_new_time != 0 && _new_time == block.timestamp)) {
             updating_task.task_message = _new_msg;
             TasksOwnershipList[msg.sender][task_idx].task_message = _new_msg;
             updating_task.task_complete_period = _new_time;
             TasksOwnershipList[msg.sender][task_idx].task_complete_period = _new_time;
-            updated=true;
+            updated = true;
         }
 
         updated = false;
         revert("There is no new value for update");
     }
 
-    function CompleteTask(uint _task_id_for_complete, uint8 _v, bytes32 _r, bytes32 _s) external onlyTaskOwner(_task_id_for_complete, _v, _r, _s) returns(bool completed) {
-        require(TaskIdActivate[_task_id_for_complete] == true, "Task has already canceled");
+    // function CompleteTask(uint _task_id_for_complete, uint8 _v, bytes32 _r, bytes32 _s) external onlyTaskOwner(_task_id_for_complete, _v, _r,) returns(bool completed) {
+    //     require(TaskIdActivate[_task_id_for_complete] == true, "Task has already canceled");
 
-    }
+    // }
 
-    function _GetTaskCompletedReward(address _to, uint _amount) internal onlyOwner nonReentrant returns (bool rewarded) {
+    function _GetTaskCompletedReward(address _to, uint256 _amount)
+        internal
+        onlyOwner
+        nonReentrant
+        returns (bool rewarded)
+    {
         require(_to != address(0) && _amount > 0, "invalid inputs");
         TaskOwnerBudget[msg.sender] = TaskOwnerBudget[msg.sender].add(CONST_REWARD_AMOUNT);
 
